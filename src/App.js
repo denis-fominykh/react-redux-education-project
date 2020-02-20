feb786e730d56bdf230e21d884f71fe41bc47e58
denis-fominykh <fominykhdenis@gmail.com>
2020-02-20 20:10:19 +0200

    creating the postform component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import './App.scss';
 
 import { Posts } from './components/Posts';
+import { Postform } from './components/Postform';
 
 export function App() {
   return (
     <div className='App'>
+      <Postform />
+      <hr />
       <Posts />
     </div>
   );

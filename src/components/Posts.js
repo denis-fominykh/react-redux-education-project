696ec027d6abcc527e38bc1a064d5a0f0a7d068b
denis-fominykh <fominykhdenis@gmail.com>
2020-02-21 00:46:28 +0200

    extending the post actions

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,17 +1,6 @@
 import React, { Component } from 'react';
 
 export class Posts extends Component {
-  state = {
-    posts: [],
-  };
-
-  UNSAFE_componentWillMount() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(res => res.json())
-      .then(data => this.setState({ posts: data }))
-      .catch(error => console.log(error));
-  }
-
   render() {
     const postItem = this.state.posts.map(post => {
       return (

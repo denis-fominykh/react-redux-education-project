d2f7a8acdc4e460f50ecc3df47d21b9fd7226e26
denis-fominykh <fominykhdenis@gmail.com>
2020-02-20 20:47:49 +0200

    connection a redux to the project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import './App.scss';
 
 import { Posts } from './components/Posts';
 import { Postform } from './components/Postform';
 
+const store = createStore(() => [], {}, applyMiddleware());
+
 export function App() {
   return (
-    <div className='App'>
-      <Postform />
-      <hr />
-      <Posts />
-    </div>
+    <Provider store={store}>
+      <div className='App'>
+        <Postform />
+        <hr />
+        <Posts />
+      </div>
+    </Provider>
   );
 }
